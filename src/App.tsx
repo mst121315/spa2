4ec91a6e6a0d737c10392a8d96e5def558ed68bf
Marina Sazonova <mst121315@gmail.com>
2025-11-25 09:23:16 +0100

    Add basename

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/spa2">
       <div className="app">
         <header className="header">
           <div className="header-content">
